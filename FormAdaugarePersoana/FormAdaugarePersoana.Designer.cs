b43342ef3fa36c831aa135133b96c75c86b70254
croit <croit@DESKTOP-BENI>
2022-05-18 13:36:41 +0300

    + Validari Adaugare Persoana

--- FormAdaugarePersoana/FormAdaugarePersoana.Designer.cs
+++ FormAdaugarePersoana/FormAdaugarePersoana.Designer.cs
@@ -39,10 +39,14 @@
             this.txtNr = new System.Windows.Forms.TextBox();
             this.txtSerie = new System.Windows.Forms.TextBox();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.lblEroareSerie = new System.Windows.Forms.Label();
+            this.lblEroarePrenume = new System.Windows.Forms.Label();
+            this.lblEroareNume = new System.Windows.Forms.Label();
             this.btnSterge = new System.Windows.Forms.Button();
             this.btnAdaug = new System.Windows.Forms.Button();
             this.directorySearcher1 = new System.DirectoryServices.DirectorySearcher();
-            this.label1 = new System.Windows.Forms.Label();
+            this.lblEroareCNP = new System.Windows.Forms.Label();
+            this.lblEroareNr = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -50,10 +54,9 @@
             // 
             this.lblNume.AutoSize = true;
             this.lblNume.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblNume.Location = new System.Drawing.Point(11, 18);
-            this.lblNume.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblNume.Location = new System.Drawing.Point(15, 22);
             this.lblNume.Name = "lblNume";
-            this.lblNume.Size = new System.Drawing.Size(70, 22);
+            this.lblNume.Size = new System.Drawing.Size(83, 27);
             this.lblNume.TabIndex = 0;
             this.lblNume.Text = "Nume:";
             // 
@@ -61,10 +64,9 @@
             // 
             this.lblPrenume.AutoSize = true;
             this.lblPrenume.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblPrenume.Location = new System.Drawing.Point(233, 19);
-            this.lblPrenume.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblPrenume.Location = new System.Drawing.Point(311, 23);
             this.lblPrenume.Name = "lblPrenume";
-            this.lblPrenume.Size = new System.Drawing.Size(101, 22);
+            this.lblPrenume.Size = new System.Drawing.Size(118, 27);
             this.lblPrenume.TabIndex = 0;
             this.lblPrenume.Text = "Prenume:";
             // 
@@ -72,10 +74,9 @@
             // 
             this.lblCNP.AutoSize = true;
             this.lblCNP.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblCNP.Location = new System.Drawing.Point(11, 75);
-            this.lblCNP.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblCNP.Location = new System.Drawing.Point(15, 109);
             this.lblCNP.Name = "lblCNP";
-            this.lblCNP.Size = new System.Drawing.Size(57, 22);
+            this.lblCNP.Size = new System.Drawing.Size(68, 27);
             this.lblCNP.TabIndex = 0;
             this.lblCNP.Text = "CNP:";
             // 
@@ -83,10 +84,9 @@
             // 
             this.lblSerie.AutoSize = true;
             this.lblSerie.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblSerie.Location = new System.Drawing.Point(10, 140);
-            this.lblSerie.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblSerie.Location = new System.Drawing.Point(13, 186);
             this.lblSerie.Name = "lblSerie";
-            this.lblSerie.Size = new System.Drawing.Size(89, 22);
+            this.lblSerie.Size = new System.Drawing.Size(104, 27);
             this.lblSerie.TabIndex = 0;
             this.lblSerie.Text = "Serie CI:";
             // 
@@ -94,20 +94,19 @@
             // 
             this.lblNr.AutoSize = true;
             this.lblNr.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblNr.Location = new System.Drawing.Point(177, 139);
-            this.lblNr.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblNr.Location = new System.Drawing.Point(236, 185);
             this.lblNr.Name = "lblNr";
-            this.lblNr.Size = new System.Drawing.Size(62, 22);
+            this.lblNr.Size = new System.Drawing.Size(73, 27);
             this.lblNr.TabIndex = 0;
             this.lblNr.Text = "Nr CI:";
             // 
             // txtNume
             // 
             this.txtNume.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtNume.Location = new System.Drawing.Point(85, 15);
-            this.txtNume.Margin = new System.Windows.Forms.Padding(2);
+            this.txtNume.Location = new System.Drawing.Point(113, 18);
+            this.txtNume.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtNume.Name = "txtNume";
-            this.txtNume.Size = new System.Drawing.Size(120, 29);
+            this.txtNume.Size = new System.Drawing.Size(159, 34);
             this.txtNume.TabIndex = 1;
             this.txtNume.TextChanged += new System.EventHandler(this.txtNume_TextChanged);
             this.txtNume.Validating += new System.ComponentModel.CancelEventHandler(this.txtNume_Validating);
@@ -115,43 +114,51 @@
             // txtPrenume
             // 
             this.txtPrenume.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtPrenume.Location = new System.Drawing.Point(339, 16);
-            this.txtPrenume.Margin = new System.Windows.Forms.Padding(2);
+            this.txtPrenume.Location = new System.Drawing.Point(452, 20);
+            this.txtPrenume.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtPrenume.Name = "txtPrenume";
-            this.txtPrenume.Size = new System.Drawing.Size(120, 29);
+            this.txtPrenume.Size = new System.Drawing.Size(159, 34);
             this.txtPrenume.TabIndex = 2;
+            this.txtPrenume.Validating += new System.ComponentModel.CancelEventHandler(this.txtPrenume_Validating);
             // 
             // txtCNP
             // 
             this.txtCNP.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtCNP.Location = new System.Drawing.Point(75, 73);
-            this.txtCNP.Margin = new System.Windows.Forms.Padding(2);
+            this.txtCNP.Location = new System.Drawing.Point(100, 107);
+            this.txtCNP.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtCNP.Name = "txtCNP";
-            this.txtCNP.Size = new System.Drawing.Size(176, 29);
+            this.txtCNP.Size = new System.Drawing.Size(233, 34);
             this.txtCNP.TabIndex = 3;
+            this.txtCNP.Validating += new System.ComponentModel.CancelEventHandler(this.txtCNP_Validating);
             // 
             // txtNr
             // 
             this.txtNr.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtNr.Location = new System.Drawing.Point(237, 136);
-            this.txtNr.Margin = new System.Windows.Forms.Padding(2);
+            this.txtNr.Location = new System.Drawing.Point(316, 181);
+            this.txtNr.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtNr.Name = "txtNr";
-            this.txtNr.Size = new System.Drawing.Size(86, 29);
+            this.txtNr.Size = new System.Drawing.Size(113, 34);
             this.txtNr.TabIndex = 5;
+            this.txtNr.Validating += new System.ComponentModel.CancelEventHandler(this.txtNr_Validating);
             // 
             // txtSerie
             // 
             this.txtSerie.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtSerie.Location = new System.Drawing.Point(100, 136);
-            this.txtSerie.Margin = new System.Windows.Forms.Padding(2);
+            this.txtSerie.Location = new System.Drawing.Point(133, 181);
+            this.txtSerie.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtSerie.Name = "txtSerie";
-            this.txtSerie.Size = new System.Drawing.Size(36, 29);
+            this.txtSerie.Size = new System.Drawing.Size(47, 34);
             this.txtSerie.TabIndex = 4;
+            this.txtSerie.Validating += new System.ComponentModel.CancelEventHandler(this.txtSerie_Validating);
             // 
             // panel1
             // 
             this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.lblEroareNr);
+            this.panel1.Controls.Add(this.lblEroareCNP);
+            this.panel1.Controls.Add(this.lblEroareSerie);
+            this.panel1.Controls.Add(this.lblEroarePrenume);
+            this.panel1.Controls.Add(this.lblEroareNume);
             this.panel1.Controls.Add(this.txtSerie);
             this.panel1.Controls.Add(this.txtNr);
             this.panel1.Controls.Add(this.txtCNP);
@@ -162,19 +169,58 @@
             this.panel1.Controls.Add(this.lblCNP);
             this.panel1.Controls.Add(this.lblPrenume);
             this.panel1.Controls.Add(this.lblNume);
-            this.panel1.Location = new System.Drawing.Point(10, 16);
-            this.panel1.Margin = new System.Windows.Forms.Padding(2);
+            this.panel1.Location = new System.Drawing.Point(13, 20);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(480, 197);
+            this.panel1.Size = new System.Drawing.Size(639, 253);
             this.panel1.TabIndex = 2;
+            // 
+            // lblEroareSerie
+            // 
+            this.lblEroareSerie.AutoSize = true;
+            this.lblEroareSerie.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEroareSerie.ForeColor = System.Drawing.Color.Red;
+            this.lblEroareSerie.Location = new System.Drawing.Point(130, 217);
+            this.lblEroareSerie.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblEroareSerie.Name = "lblEroareSerie";
+            this.lblEroareSerie.Size = new System.Drawing.Size(86, 15);
+            this.lblEroareSerie.TabIndex = 7;
+            this.lblEroareSerie.Text = "*Serie invalida";
+            this.lblEroareSerie.Visible = false;
+            // 
+            // lblEroarePrenume
+            // 
+            this.lblEroarePrenume.AutoSize = true;
+            this.lblEroarePrenume.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEroarePrenume.ForeColor = System.Drawing.Color.Red;
+            this.lblEroarePrenume.Location = new System.Drawing.Point(449, 57);
+            this.lblEroarePrenume.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblEroarePrenume.Name = "lblEroarePrenume";
+            this.lblEroarePrenume.Size = new System.Drawing.Size(168, 30);
+            this.lblEroarePrenume.TabIndex = 6;
+            this.lblEroarePrenume.Text = "*Prenumele nu poate contine\r\ncifre, spații sau alte simboluri!";
+            this.lblEroarePrenume.Visible = false;
+            // 
+            // lblEroareNume
+            // 
+            this.lblEroareNume.AutoSize = true;
+            this.lblEroareNume.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEroareNume.ForeColor = System.Drawing.Color.Red;
+            this.lblEroareNume.Location = new System.Drawing.Point(110, 57);
+            this.lblEroareNume.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblEroareNume.Name = "lblEroareNume";
+            this.lblEroareNume.Size = new System.Drawing.Size(168, 30);
+            this.lblEroareNume.TabIndex = 6;
+            this.lblEroareNume.Text = "*Numele nu poate contine\r\ncifre, spații sau alte simboluri!";
+            this.lblEroareNume.Visible = false;
             // 
             // btnSterge
             // 
             this.btnSterge.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSterge.Location = new System.Drawing.Point(274, 235);
-            this.btnSterge.Margin = new System.Windows.Forms.Padding(2);
+            this.btnSterge.Location = new System.Drawing.Point(365, 292);
+            this.btnSterge.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnSterge.Name = "btnSterge";
-            this.btnSterge.Size = new System.Drawing.Size(92, 33);
+            this.btnSterge.Size = new System.Drawing.Size(123, 41);
             this.btnSterge.TabIndex = 6;
             this.btnSterge.Text = "Sterge";
             this.btnSterge.UseVisualStyleBackColor = true;
@@ -183,10 +229,10 @@
             // btnAdaug
             // 
             this.btnAdaug.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAdaug.Location = new System.Drawing.Point(398, 235);
-            this.btnAdaug.Margin = new System.Windows.Forms.Padding(2);
+            this.btnAdaug.Location = new System.Drawing.Point(531, 292);
+            this.btnAdaug.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnAdaug.Name = "btnAdaug";
-            this.btnAdaug.Size = new System.Drawing.Size(92, 33);
+            this.btnAdaug.Size = new System.Drawing.Size(123, 41);
             this.btnAdaug.TabIndex = 7;
             this.btnAdaug.Text = "Adauga";
             this.btnAdaug.UseVisualStyleBackColor = true;
@@ -198,28 +244,43 @@
             this.directorySearcher1.ServerPageTimeLimit = System.TimeSpan.Parse("-00:00:01");
             this.directorySearcher1.ServerTimeLimit = System.TimeSpan.Parse("-00:00:01");
             // 
-            // label1
+            // lblEroareCNP
             // 
-            this.label1.AutoSize = true;
-            this.label1.ForeColor = System.Drawing.Color.Red;
-            this.label1.Location = new System.Drawing.Point(79, 50);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(156, 13);
-            this.label1.TabIndex = 6;
-            this.label1.Text = "*Numele nu poate contine cifre!";
-            this.label1.Visible = false;
+            this.lblEroareCNP.AutoSize = true;
+            this.lblEroareCNP.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEroareCNP.ForeColor = System.Drawing.Color.Red;
+            this.lblEroareCNP.Location = new System.Drawing.Point(97, 143);
+            this.lblEroareCNP.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblEroareCNP.Name = "lblEroareCNP";
+            this.lblEroareCNP.Size = new System.Drawing.Size(75, 15);
+            this.lblEroareCNP.TabIndex = 7;
+            this.lblEroareCNP.Text = "*CNP invalid";
+            this.lblEroareCNP.Visible = false;
+            // 
+            // lblEroareNr
+            // 
+            this.lblEroareNr.AutoSize = true;
+            this.lblEroareNr.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEroareNr.ForeColor = System.Drawing.Color.Red;
+            this.lblEroareNr.Location = new System.Drawing.Point(316, 217);
+            this.lblEroareNr.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblEroareNr.Name = "lblEroareNr";
+            this.lblEroareNr.Size = new System.Drawing.Size(88, 15);
+            this.lblEroareNr.TabIndex = 7;
+            this.lblEroareNr.Text = "*Numar invalid";
+            this.lblEroareNr.Visible = false;
             // 
             // FormAdaugarePersoana
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(506, 293);
+            this.ClientSize = new System.Drawing.Size(672, 351);
             this.Controls.Add(this.btnAdaug);
             this.Controls.Add(this.btnSterge);
             this.Controls.Add(this.panel1);
-            this.Margin = new System.Windows.Forms.Padding(2);
-            this.MaximumSize = new System.Drawing.Size(522, 332);
-            this.MinimumSize = new System.Drawing.Size(522, 332);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.MaximumSize = new System.Drawing.Size(690, 398);
+            this.MinimumSize = new System.Drawing.Size(690, 398);
             this.Name = "FormAdaugarePersoana";
             this.Text = "Înregistrează persoană";
             this.Load += new System.EventHandler(this.FormAdaugarePersoana_Load);
@@ -245,7 +306,11 @@
         private System.Windows.Forms.Button btnSterge;
         private System.Windows.Forms.Button btnAdaug;
         private System.DirectoryServices.DirectorySearcher directorySearcher1;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblEroareNume;
+        private System.Windows.Forms.Label lblEroarePrenume;
+        private System.Windows.Forms.Label lblEroareSerie;
+        private System.Windows.Forms.Label lblEroareNr;
+        private System.Windows.Forms.Label lblEroareCNP;
     }
 }
 
--- FormAdaugarePersoana/FormAdaugarePersoana.cs
+++ FormAdaugarePersoana/FormAdaugarePersoana.cs
@@ -13,6 +13,79 @@ namespace FormAdaugarePersoana
 {
     public partial class FormAdaugarePersoana : Form
     {
+        private static Dictionary<int, string> seriiBuletin = new Dictionary<int, string>()
+        {
+            {0, "AX" },
+            {1, "TR" },
+            {2, "AR" },
+            {3, "ZR" },
+            {4, "XC" },
+            {5, "ZC" },
+            {6, "MM" },
+            {7, "XM" },
+            {8, "XB" },
+            {9, "XT" },
+            {10, "BV" },
+            {11, "ZV" },
+            {12, "XR" },
+            {13, "DP" },
+            {14, "DR" },
+            {15, "DT" },
+            {16, "DX" },
+            {17, "RD" },
+            {18, "RR" },
+            {19, "RT" },
+            {20, "RX" },
+            {21, "RK" },
+            {22, "IF" },
+            {23, "XZ" },
+            {24, "KL" },
+            {25, "KX" },
+            {26, "CJ" },
+            {27, "KT" },
+            {28, "KZ" },
+            {29, "DX" },
+            {30, "DZ" },
+            {31, "HD" },
+            {32, "XD" },
+            {33, "VN" },
+            {34, "GL" },
+            {35, "ZL" },
+            {36, "GG" },
+            {37, "MX" },
+            {38, "MZ" },
+            {39, "IZ" },
+            {40, "MH" },
+            {41, "HR" },
+            {42, "XH" },
+            {43, "ZH" },
+            {44, "NT" },
+            {45, "AS" },
+            {46, "AZ" },
+            {47, "PH" },
+            {48, "PX" },
+            {49, "KS" },
+            {50, "VX" },
+            {51, "SM" },
+            {52, "KV" },
+            {53, "SB" },
+            {54, "OT" },
+            {55, "SZ" },
+            {56, "SV" },
+            {57, "XV" },
+            {58, "TM" },
+            {59, "TZ" },
+            {60, "DD" },
+            {61, "GZ" },
+            {62, "ZS" },
+            {63, "MS" },
+            {64, "TC" },
+            {65, "VS" },
+            {66, "SX" }
+        };
+        
+
+
         public FormAdaugarePersoana()
         {
             this.CenterToScreen();
@@ -26,6 +99,11 @@ namespace FormAdaugarePersoana
             txtNume.Clear();
             txtPrenume.Clear();
             txtSerie.Clear();
+            lblEroareNume.Visible = false;
+            lblEroarePrenume.Visible = false;
+            lblEroareCNP.Visible = false;
+            lblEroareSerie.Visible = false;
+            lblEroareNr.Visible = false;
         }
 
         private void btnAdaug_Click(object sender, EventArgs e)
@@ -33,14 +111,19 @@ namespace FormAdaugarePersoana
             if (txtCNP.Text == string.Empty || txtNr.Text == string.Empty || txtNume.Text == string.Empty || txtPrenume.Text == string.Empty || txtSerie.Text == string.Empty)
             {
                 DialogResult res = MessageBox.Show("Cel putin un camp este gol", "EROARE", MessageBoxButtons.OK);
-                
             }
             else
             {
-                int.TryParse(txtNr.Text, out int nr);
-                ClasaPersoana.Persoana persoana = new Persoana(-1, txtNume.Text, txtPrenume.Text, txtCNP.Text, txtSerie.Text, nr);
-                persoana.SalvarePersoana();
+                if (lblEroareNume.Visible == true || lblEroarePrenume.Visible == true || lblEroareCNP.Visible == true || lblEroareSerie.Visible == true || lblEroareNr.Visible == true)
+                { DialogResult eroare = MessageBox.Show("Completati toate campurile corect", "EROARE", MessageBoxButtons.OK); }
+                else
+                {
+                    int.TryParse(txtNr.Text, out int nr);
+                    ClasaPersoana.Persoana persoana = new Persoana(-1, txtNume.Text, txtPrenume.Text, txtCNP.Text, txtSerie.Text, nr);
+                    persoana.SalvarePersoana();
+                }
             }
+                
             
         }
 
@@ -54,13 +137,68 @@ namespace FormAdaugarePersoana
 
         }
 
+
+        // Validari campuri text ========================================================
         private void txtNume_Validating(object sender, CancelEventArgs e)
         {
             if (!txtNume.Text.All(char.IsLetter))
-                label1.Visible = true;
+                lblEroareNume.Visible = true;
             else
-                label1.Visible = false;
-            
+                lblEroareNume.Visible = false;
+            if (txtNume.Text == string.Empty)
+                lblEroareNr.Visible = false;
+                        
         }
+
+        private void txtPrenume_Validating(object sender, CancelEventArgs e)
+        {
+            if (!txtPrenume.Text.All(char.IsLetter))
+                lblEroarePrenume.Visible = true;
+            else
+                lblEroarePrenume.Visible = false;
+            if (txtPrenume.Text == string.Empty)
+                lblEroarePrenume.Visible = false;
+        }
+
+        private void txtSerie_Validating(object sender, CancelEventArgs e)
+        {
+            int val = 0;
+            for (int i = 0; i <= 66; i++)
+                if (txtSerie.Text == seriiBuletin[i])
+                {
+                    val = 1;
+                    break;
+                }
+            if(val == 0)
+                lblEroareSerie.Visible = true;
+            else lblEroareSerie.Visible = false;
+
+            if (txtSerie.Text == string.Empty)
+                lblEroareSerie.Visible = false;
+
+        }
+
+        private void txtCNP_Validating(object sender, CancelEventArgs e)
+        {
+            if ( txtCNP.Text.Length != 13 || !txtCNP.Text.All(char.IsDigit))
+                lblEroareCNP.Visible = true;
+            else
+                lblEroareCNP.Visible = false;
+
+            if (txtCNP.Text == string.Empty)
+                lblEroareCNP.Visible = false;
+        }
+
+        private void txtNr_Validating(object sender, CancelEventArgs e)
+        {
+            if ( txtNr.Text.Length != 6 || !txtNr.Text.All(char.IsDigit))
+                lblEroareNr.Visible = true;
+            else
+                 lblEroareNr.Visible = false;
+
+            if (txtNr.Text == string.Empty)
+                lblEroareNr.Visible = false;
+        }
+        // =============================================================================
     }
 }
